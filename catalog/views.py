0025cd432959ec15f0a9c80f02428cb1308f4cdf
0yah <highmeme10@gmail.com>
2021-02-25 09:24:49 +0300

    Added anonymous session

--- catalog/views.py
+++ catalog/views.py
@@ -14,8 +14,10 @@ def index(request):
 
     #Get the number of available books (Status = 'a')
     num_book_instances_available = BookInstance.objects.filter(status__exact='a').count()
-    
-    
+    num_visits = request.session.get('num_visits',1)
+    request.session['num_visits'] = num_visits + 1
+
+
     #Dictonary that stores variables that can be used by the template
 
     context = {
@@ -24,7 +26,8 @@ def index(request):
         'num_book_instances_available': num_book_instances_available,
         'num_authors': num_authors,
         'num_sci_fi_genre':scifi_num_genre,
-        'title':'Local Library'
+        'title':'Local Library',
+        'num_visits':num_visits,
     }
     return render(request,'catalog/index.html',context=context)
 
